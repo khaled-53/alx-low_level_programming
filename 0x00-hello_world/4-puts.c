a5761366ac17fe68a9e10d3f6c9da677db63941c
Khaled <kcracker53@gmail.com>
2023-12-14 16:16:31 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the usage of the puts function
+ * to print a string to the standard output.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
