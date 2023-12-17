62dbae21b61552cef87ff47b41954bbccc024405
Khaled <kcracker53@gmail.com>
2023-12-17 09:50:45 -0800

    wow

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
-
+/**
+ * main - entry point
+ *
+ * Description: i am not sure
+ *
+ * Return: (1)
+ */
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
