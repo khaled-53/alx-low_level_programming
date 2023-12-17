218a5beed97021bef2489016e72bcc368fb738b3
Khaled <kcracker53@gmail.com>
2023-12-17 06:01:52 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - Entry point of the program
- *   *
- *    * Description: This program demonstrates the usage of the puts function
- *     * to print a string to the standard output.
- *      *
- *       * Return: Always 0 (success)
- *        */
+* main - Entry point of the program
+* Description: This program demonstrates the usage of the puts function
+* to print a string to the standard output.
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
