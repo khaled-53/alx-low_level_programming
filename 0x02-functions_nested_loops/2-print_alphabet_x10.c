6e4d4faa514769010bdd9191f50fba725ebf2bda
Khaled <kcracker53@gmail.com>
2023-12-20 08:45:42 -0800

    yeaaai

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10() - pcrint x10
+ * print_alphabet_x10 - pcrint x10
+ * Description: read aove
  */
 void print_alphabet_x10(void)
 {
 	char i = 'a';
-	
+
 	int x = 0;
 
 	for (x = 0; x < 10; x++)
