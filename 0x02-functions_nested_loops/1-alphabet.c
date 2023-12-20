e4dfffa87b2d9d47e66954ba08491199aca8a1e6
Khaled <kcracker53@gmail.com>
2023-12-20 04:33:24 -0800

    tr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * main - entry point
+ * Description: welo
+ * Return: (0)
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	return (0);
+}
