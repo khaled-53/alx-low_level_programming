5bead5f5cf05efab3ab031e1134e4153d5047c92
Khaled <kcracker53@gmail.com>
2023-12-20 12:52:04 -0800

    ff

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 void times_table(void) {
    int num, mult, prod;
 
    for (num = 0; num <= 9; num++) {
-       _putchar('0');  // Print the initial '0'
+       _putchar('0');
 
        for (mult = 1; mult <= 9; mult++) {
            _putchar(',');
@@ -13,14 +13,14 @@ void times_table(void) {
            prod = num * mult;
 
            if (prod <= 9) {
-               _putchar(' ');  // Align single-digit products
+               _putchar(' ');
            } else {
-               _putchar((prod / 10) + '0');  // Print tens digit
+               _putchar((prod / 10) + '0');
            }
 
-           _putchar((prod % 10) + '0');  // Print ones digit
+           _putchar((prod % 10) + '0');
        }
 
-       _putchar('\n');  // Move to the next line
+       _putchar('\n'); 
    }
 }
