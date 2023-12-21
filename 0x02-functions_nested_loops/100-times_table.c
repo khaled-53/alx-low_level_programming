56653140b4a49cd4fbe290cc9ce55d8cb38b82cd
Khaled <kcracker53@gmail.com>
2023-12-21 10:52:54 -0800

    fdfgf

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_times_table - Prints a multiplication table up to a specified number.
+ * @n: The highest number for the multiplication table (1 to 15 inclusive).
+ */
+void print_times_table(int n)
+{
+	if (n <= 0 || n > 15)
+	return;
+	int num, mult, prod;
+
+	for (num = 0; num <= n; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= n; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
