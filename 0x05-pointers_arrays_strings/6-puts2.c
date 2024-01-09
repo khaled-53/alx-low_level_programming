7faf0ca2226d56f9ba38ba87409f7628dd9160d8
Khaled <kcracker53@gmail.com>
2024-01-09 07:47:27 -0800

    ffhh

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+}
